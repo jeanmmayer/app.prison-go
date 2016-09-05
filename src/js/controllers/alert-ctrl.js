a968cd527738bbd11009d596e1bdc8356bd03aa8
Douglas Wille <willedouglas10@gmail.com>
2016-09-05 19:30:43 -0300

    Arquivos iniciais do projeto.
    Arquivos iniciais do projeto.

---
+++ src/js/controllers/alert-ctrl.js
@@ -0,0 +1,27 @@
+/**
+ * Alerts Controller
+ */
+
+angular
+    .module('RDash')
+    .controller('AlertsCtrl', ['$scope', AlertsCtrl]);
+
+function AlertsCtrl($scope) {
+    $scope.alerts = [{
+        type: 'success',
+        msg: 'A meta de visitas por dia - foi cumprida.'
+    }, {
+        type: 'danger',
+        msg: 'Existem detentos com informações incompletas'
+    }];
+
+    $scope.addAlert = function() {
+        $scope.alerts.push({
+            msg: 'Another alert!'
+        });
+    };
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+}
